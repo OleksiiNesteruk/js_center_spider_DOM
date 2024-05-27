adec4ccffcd2df114af1b4f1dae396fa7738843a
OleksiiNesteruk <nesteruk.oleksiy@gmail.com>
2024-05-27 13:08:48 +0100

    make build

---
+++ index.dd79d2a9.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientWidth/2,n=e.clientHeight/2,l=t.clientWidth/2,i=t.clientHeight/2;t.style="\n  top: ".concat(n-i,"px;\n  left: ").concat(c-l,"px;\n");
+//# sourceMappingURL=index.dd79d2a9.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const centerX = wall.clientWidth / 2;
+const centerY = wall.clientHeight / 2;
+const centerXSpider = spider.clientWidth / 2;
+const centerYSpider = spider.clientHeight / 2;
+spider.style = `
+  top: ${centerY - centerYSpider}px;
+  left: ${centerX - centerXSpider}px;
+`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
